d024a5f819be54d6358936ff25d81aea6286916d
Anil951 <21211a6711@bvrit.ac.in>
2024-01-13 12:57:31 +0530

    added

---
+++ config.js
@@ -0,0 +1,97 @@
+window.PorfolioConfig = {
+    name: 'Anil Kumar Borige',
+    jobTitle: 'Data Scientist',
+    jobDescription: 'A  Self-driven, quick starter, passionate programmer with a curious mind who enjoys solving a complex and challenging real-world problems.I enjoy problem-solving and coding. Always strive to bring 100% to the work I do.',
+    skills: [
+      {
+        name: 'Programming Languages',
+        stacks:'C, Python, Java'
+      },
+      {
+        name: 'DataScience',
+        stacks:'Data Prerocessing, Analysis, Visualisation'
+      },
+      {
+        name: 'Web Development',
+        stacks:'HTML, CSS, JS'
+      },
+      {
+        name: 'Machine Learning',
+        stacks:'NumPy, Matplotlib, Pandas, Scikit-Learn'
+      },
+      {
+        name: 'Natural Language Processing',
+        stacks: 'LSTM, NLTK, spaCy'
+      },
+      {
+        name: 'Database Management | Frameworks',
+        stacks:'mySQL | Flask'
+      },
+     
+    ],
+    aboutme:[
+      {
+        description: 'I am Anilkumar Borige a computer science undergraduate from Hyderabad, Telangana, India. A Self-driven, quick starter, passionate programmer with a curious mind who enjoys solving a complex and challenging real-world problems. I enjoy problem-solving and coding. Always strive to bring 100% to the work I do. ',
+        interests1: '- interested in coding related stuff ',
+        interests2: '- open to new ideas',
+        interests3: '- swimming ',
+        interests4: '- sleeping',
+        fp1: '- data science ',
+        fp2: '- machine learning',
+        fp3: '- internship opputurtunity ',
+        fp4: '- full time placement',
+      },
+     
+    ],
+
+    projects: [
+      {
+        name: 'TY Rank - Youtube Video Recommendation for Domain Specific Content',
+        link: 'https://github.com/Anil951/YouTube-Video-Recommendation-for-Domain-Specific-Content',
+        hostedLink: 'https://github.com/Anil951/YouTube-Video-Recommendation-for-Domain-Specific-Content',
+        description: 'Delivering Domain-specific YouTube video recommendations for first-time viewers through sentiment analysis on comments using an ensemble of multiple models. The system also includes spam filtering on comments and video ranking based on normalized scores, Primarily catering to first-time viewers.',
+        stack: 'NLP|LSTM|Python|YouTube Data API|Data Science'
+      },
+      {
+        name: 'DiagnoGuide ',
+        link: 'https://diagnoguide.onrender.com/',
+        hostedLink: 'https://diagnoguide.onrender.com/',
+        description: 'A Machine Learning-based system for disease prediction and personalized medicine recommendations. This comprehensive platform includes disease descriptions, precautionary measures, and integrated geolocation services to identify nearby hospitals for enhanced accessibility and healthcare support.',
+        stack: ' ML|Flask|Python|HTML|CSS|JS|AJAX|Data Science'
+      },
+      {
+        name: 'ShelterGuide',
+        link: 'https://shelterguide.onrender.com/',
+        hostedLink: 'https://shelterguide.onrender.com/',
+        description: 'A Machine Learning platform designed to predict house and rent prices while providing visual representations of housing locations on maps across six major cities in India. The platform incorporates essential amenities and nearby resources, enhancing the overall understanding of the housing landscape.',
+        stack: ' ML|Flask|Python|HTML|CSS|JS|Data Science'
+      },
+      {
+        name: 'Camera',
+        link: 'https://anil951.github.io/camera/',
+        hostedLink:'https://anil951.github.io/camera/',
+        description: 'An all-in-one Camera Application designed for versatile use on any device. This application allows users to capture photos and videos seamlessly, featuring a temporary gallery option with the ability to delete or download content. Additionally, it offers a one-time-use functionality, making it suitable for various appliances.',
+        stack: ' HTML|CSS|JS'
+      }
+    ],
+   
+    contacts: {
+      email: 'anilkumarborige@gmail.com',
+      mobileNo: '+918688459201',
+      phoneNo: 'Hyderabad, Telangana',
+      links: [
+        {
+          name: 'GitHub',
+          link: 'https://github.com/Anil951',
+        },
+        {
+          name: 'Linkedin',
+          link: 'https://www.linkedin.com/in/anilkumarborige/'
+        },
+        {
+          name: 'Instagram',
+          link: 'https://www.instagram.com/thisis__anil/'
+        }
+      ]
+    }
+  };
